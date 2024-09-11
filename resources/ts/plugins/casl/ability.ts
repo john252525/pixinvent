675668051b636b1f3f14d7812e68f2cff169c295
Sergey <klips@live.ru>
2024-09-11 16:11:45 +0300

    Add Webhook catch service

--- resources/ts/plugins/casl/ability.ts
+++ resources/ts/plugins/casl/ability.ts
@@ -3,7 +3,7 @@ import { createMongoAbility } from '@casl/ability'
 export type Actions = 'create' | 'read' | 'update' | 'delete' | 'manage'
 
 // ex: Post, Comment, User, etc. We haven't used any of these in our demo though.
-export type Subjects = 'admin' | 'user' | 'premium' | 'all'
+export type Subjects = 'admin' | 'user' | 'domain1' | 'domain2' | 'domain3' | 'all'
 
 export interface Rule { action: Actions; subject: Subjects }
 
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,20 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('{any?}', function() {
+Route::get('{any?}', function () {
     return view('application');
 })->where('any', '.*');
+
+Route::any('webhooks/{service}/{type?}', function (Request $request, string $service, ?string $type = null) {
+    try {
+        \App\Models\Webhook::create(['service' => $service, 'type' => $type, 'payload' => $request->all()]);
+        return response()->json('ok');
+    } catch (\Exception $e) {
+        \Log::error('Webhook error: '.$service.' Message: '.$e->getMessage().' Payload: '.json_encode($request->all()));
+        return response()->json('ok');
+    }
+});
 
 Route::get('auth/password/reset', [\App\Http\Controllers\Api\User\AuthController::class, 'passwordReset'])->name('password.reset');
